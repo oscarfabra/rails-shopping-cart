8f8836f734b7eb6b3231629bedf06488f7bc2989
Oscar Fabra <oscarfabra@gmail.com>
2014-11-20 23:45:46 -0500

    Created line_items model and associations between them and products and cart.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -6,7 +7,22 @@ class Product < ActiveRecord::Base
                           with: %r{\.(gif|jpg|png)\Z}i,
                           message: 'must be a URL for GIF, JPG or PNG image.'
                       }
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  # Returns latest product on the database
   def self.latest
     Product.order(:updated_at).last
   end
+
+  private
+
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
