f1738d4bce8b2f36e1a129c96469110f7bc43035
Oscar Fabra <oscarfabra@gmail.com>
2014-11-20 23:13:52 -0500

    Created Cart Controller and added CurrentCart module.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119201139) do
+ActiveRecord::Schema.define(version: 20141121034118) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
