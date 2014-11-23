fb0f6fda5cb9180a24321458ea80f2001cd355ab
Oscar Fabra <oscarfabra@gmail.com>
2014-11-22 19:32:49 -0500

    Added tests to cart model. Fixed cart model.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,7 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
 
-  # Adds a product to cart. Create a new LineItem if it doesn't exist yet.
+  # Adds a product to cart. Creates a new LineItem if it doesn't exist yet.
   def add_product(product_id)
     current_item = line_items.find_by(product_id: product_id)
     if current_item
@@ -9,7 +9,9 @@ class Cart < ActiveRecord::Base
     else
       current_item = line_items.build(product_id: product_id)
       current_item.price = current_item.product.price
+      line_items << current_item
     end
+    current_item.save!
     current_item
   end
 
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -3,12 +3,37 @@ require 'test_helper'
 class CartTest < ActiveSupport::TestCase
   fixtures :carts
 
-  test "adding different products should extend line items" do
-
+  test "adding different products should increase line items" do
+    # Create cart
+    cart = carts(:cart_1)
+    # Add three different books to cart
+    size, total_price = 0, 0
+    [:coffee, :ruby, :rails].each do |product_name|
+      product = products(product_name)
+      item = cart.add_product(product.id)
+      assert_in_delta item.price, product.price, 0.001,
+                   "cart line item price is incorrect"
+      size += 1
+      total_price += item.price
+    end
+    # Ensure line_items size increases accordingly
+    assert_equal size, cart.line_items.size
+    # Ensure cart's total price is correct
+    assert_in_delta total_price, cart.total_price, 0.001
   end
 
   test "adding same product should increase line item quantity" do
-
+    # Create cart
+    cart = carts(:cart_2)
+    # Add the same book several times to cart
+    product = products(:ruby)
+    quantity = 3
+    quantity.times { cart.add_product(product.id) }
+    line_item = cart.line_items.find_by(product_id: product.id)
+    # Ensure line_item's quantity is correct
+    assert_equal quantity, line_item.quantity, "cart line item is incorrect"
+    # Ensure cart's total price is correct
+    assert_in_delta product.price * quantity, cart.total_price, 0.001
   end
 
 end
