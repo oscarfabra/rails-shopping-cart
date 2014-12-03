5ee2f84c81a40161ca428300e6f057c7729e7098
Oscar Fabra <oscarfabra@gmail.com>
2014-12-02 19:30:18 -0500

    Added admin links in sidebar. Restrained app always to have at least one admin.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true, uniqueness: true
+
+  after_destroy :ensure_an_admin_remains
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        # Raises exception to avoid deleting last user. Rollbacks transaction.
+        raise "Can't delete last user"
+      end
+    end
 end
