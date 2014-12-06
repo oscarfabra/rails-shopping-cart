4c52e65f878929831d7ff4c89635490872a5f698
Oscar Fabra <oscarfabra@gmail.com>
2014-12-05 20:02:37 -0500

    Added currency equivalence depending on locale.

--- Gemfile
+++ Gemfile
@@ -14,6 +14,9 @@ gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
 
+# For currency conversion
+gem 'goog_currency'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 gem 'jquery-ui-rails'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,19 @@
 module ApplicationHelper
+
   def hidden_div_if(condition, attributes = {}, &block)
     if condition
       attributes["style"] = "display: none"
     end
     content_tag("div", attributes, &block)
+  end
+
+  # Converts the given amount to currency of the locale.
+  def number_to_locale(amount)
+    case I18n.locale.to_s
+      when 'es'  # Converts to colombian pesos (COP)
+        GoogCurrency.usd_to_cop(amount)
+      when 'en'
+        amount
+    end
   end
 end
