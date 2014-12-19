87fb5616b1173c62471ce2a7631e6ccaef122ac2
Oscar Fabra <oscarfabra@gmail.com>
2014-12-18 23:24:00 -0500

    Minor update.

--- app/controllers/payments_proxy_controller.rb
+++ app/controllers/payments_proxy_controller.rb
@@ -13,19 +13,17 @@ class PaymentsProxyController < ApplicationController
 
     redirect_to '/'
 
-    <<-DOC
-    respond_to do |format|
-      if order.save!
-        session[:order_id] = order.id
-        format.html { redirect_to '/payments/new.html' }
-        format.json do  # render an html page instead of a JSON response.
-          redirect_to '/payments/new.html'
-        end
-      else
-        format.html { redirect_to '/payments/new.html' }
-        format.json { render json: params, status: :unprocessable_entity }
-      end
-    end
-    DOC
+    # respond_to do |format|
+    #   if order.save!
+    #     session[:order_id] = order.id
+    #     format.html { redirect_to '/payments/new.html' }
+    #     format.json do  # render an html page instead of a JSON response.
+    #       redirect_to '/payments/new.html'
+    #     end
+    #   else
+    #     format.html { redirect_to '/payments/new.html' }
+    #     format.json { render json: params, status: :unprocessable_entity }
+    #   end
+    # end
   end
 end
