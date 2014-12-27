38dd154cc56acb9a16c81d1078c5703ae0f953f6
Oscar Fabra <oscarfabra@gmail.com>
2014-12-27 18:32:27 -0500

    Revert "Undid most changes. JS not working directly. About 2 use other template."
    This reverts commit bc7d17dfe7a4773a27bc3c91a79b666c22e91967.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,21 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# Additional stylesheets to be precompiled.
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( style-1000px.css )
+Rails.application.config.assets.precompile += %w( style-desktop.css )
+Rails.application.config.assets.precompile += %w( style-mobile.css )
+Rails.application.config.assets.precompile += %w( skel.css )
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( ie/v8.css )
+Rails.application.config.assets.precompile += %w( ie/v9.css )
+Rails.application.config.assets.precompile += %w( ie/PIE.htc )
+
+# Additional javascripts to be precompiled.
+Rails.application.config.assets.precompile += %w( ie/html5shiv.js )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( jquery.scrolly.min.js )
+Rails.application.config.assets.precompile += %w( skel.min.js )
+Rails.application.config.assets.precompile += %w( init.js )
