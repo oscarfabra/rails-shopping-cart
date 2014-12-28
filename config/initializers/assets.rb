a9c5552162d3861ae11fa409320f347c2de4e0fb
Oscar Fabra <oscarfabra@gmail.com>
2014-12-28 15:43:13 -0500

    Updating layout. JS not working.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,13 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# Precompile additional stylesheets.
+Rails.application.config.assets.precompile += %w( skel-noscript.css )
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( style-desktop.css )
+
+# Precompile additional javascripts.
+Rails.application.config.assets.precompile += %w( skel.min.js )
+Rails.application.config.assets.precompile += %w( skel-panels.min.js )
+Rails.application.config.assets.precompile += %w( init.js )
