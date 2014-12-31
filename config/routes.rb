78a544c1908b65b102e830487b505b0a24b277e2
Oscar Fabra <oscarfabra@gmail.com>
2014-12-31 18:44:44 -0500

    Updated landing feature imgs. Added temp help, about & contact pages.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root    'static_pages#home', as: 'home'
   get     'help'    =>  'static_pages#help'
   get     'about'   =>  'static_pages#about'
+  get     'contact' =>  'static_pages#contact'
 
   # get 'payments_proxy/read_response'
 
