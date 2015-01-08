d42bf24edf865c467957a6b2d789aebbdc907ed2
Oscar Fabra <oscarfabra@gmail.com>
2015-01-08 14:39:10 -0500

    Revert "Customizing layout with template."
    This reverts commit b2563c9ba055e14183d7e8a3d1c20e053c8ea74c.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,17 +6,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-
-# Additional stylesheets to be precompiled.
-Rails.application.config.assets.precompile += %w( skel.css )
-Rails.application.config.assets.precompile += %w( style.css )
-Rails.application.config.assets.precompile += %w( style-desktop.css )
-Rails.application.config.assets.precompile += %w( ie/v8.css )
-Rails.application.config.assets.precompile += %w( ie/v9.css )
-
-# Additional javascripts to be precompiled.
-Rails.application.config.assets.precompile += %w( ie/html5shiv.js )
-Rails.application.config.assets.precompile += %w( jquery.min.js )
-Rails.application.config.assets.precompile += %w( jquery.scrolly.min.js )
-Rails.application.config.assets.precompile += %w( skel.min.js )
-Rails.application.config.assets.precompile += %w( init.js )
